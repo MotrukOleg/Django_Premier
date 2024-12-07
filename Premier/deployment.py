28867c0e67689036b4fa81fc5fc3d71e5471a9d5
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2024-12-07 13:44:49 +0200

    update

--- Premier/deployment.py
+++ Premier/deployment.py
@@ -22,7 +22,7 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-connection_string = os.environ['AZURE_POSTGRES_CONNECTIONSTRING']
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parametrs = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
