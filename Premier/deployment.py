e1345680bec4fbb5c77df55e7ef3e89cd5037fcd
Motruk Oleg <oleh.motruk.shi.2023@lpnu.ua>
2024-12-09 23:12:09 +0200

    update

--- Premier/deployment.py
+++ Premier/deployment.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
-APPLICATIONINSIGHTS_CONNECTION_STRING = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
+APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=7b40598f-59f3-4022-85f8-651fc09040c6;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=cf56eb7c-a344-4824-b874-4dfa8a08275f"
 configure_azure_monitor(connection_string=APPLICATIONINSIGHTS_CONNECTION_STRING)
 
 
